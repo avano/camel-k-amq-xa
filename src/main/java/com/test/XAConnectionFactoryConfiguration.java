18df2e4c88797236b227d7a4a69f98d18c5fa361
Andrej Vano <avano@redhat.com>
2022-01-31 13:29:20 +0100

    [POM] Add MRRC GA repository, add jandex plugin

--- src/main/java/com/test/XAConnectionFactoryConfiguration.java
+++ src/main/java/com/test/XAConnectionFactoryConfiguration.java
@@ -1,20 +1,28 @@
 package com.test;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQXAConnectionFactory;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.narayana.jta.jms.ConnectionFactoryProxy;
 import org.jboss.narayana.jta.jms.TransactionHelperImpl;
+
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 import javax.transaction.TransactionManager;
 
-@Dependent()
+@Dependent
 public class XAConnectionFactoryConfiguration {
+    @ConfigProperty(name = "broker.url")
+    String url;
+    @ConfigProperty(name = "broker.username")
+    String user;
+    @ConfigProperty(name = "broker.password")
+    String password;
 
     @Produces()
     @Named(value = "xaConnectionFactory")
     public ConnectionFactoryProxy getXAConnectionFactory(TransactionManager tm) {
-        ActiveMQXAConnectionFactory cf = new ActiveMQXAConnectionFactory("tcp://localhost:49561", "admin", "admin123.");
+        ActiveMQXAConnectionFactory cf = new ActiveMQXAConnectionFactory(url, user, password);
         return new ConnectionFactoryProxy(cf, new TransactionHelperImpl(tm));
     }
 }
